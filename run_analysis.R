57eb89b2f02d842a17fc50974091eb03d0eea247
Sandeep Singhal <sandeepsinghal@gmail.com>
2015-02-22 17:22:45 +0530

    code to read from test subsection and write to a file

--- run_analysis.R
+++ run_analysis.R
@@ -11,11 +11,19 @@ makeTidySet <- function(dir) {
     # test/y_test.txt indicates the activity for each row in test/X_test.txt
     
     # Get all column numbers from features that match "mean" or "std"
-    featureSubset <- sqldf("select * from features where V2 LIKE '%mean%' or V2 LIKE '%std%'")[,1]
+    featureSubsetTable <- sqldf("select * from features where V2 LIKE '%mean%' or V2 LIKE '%std%'")
+    featureSubset <- featureSubsetTable[,1]
+    featureHeaders <- featureSubsetTable[,2]
 
     testX <- read.table(paste(dir, "test", "X_test.txt", sep="/"))
     testXExtracted <- testX[, featureSubset]
+    activites <- read.table(paste(dir, "test", "y_test.txt", sep="/"))
+    activityDesc <- join(activites, activityLabels)[,2]
     
-    print(featureSubset)
+    data <- cbind(testXExtracted, activityDesc)
+    columnHeadings <- append (as.character(featureHeaders), c("Activity"))
+    
+    names(data) <- columnHeadings
+    write.table(data, file = "./tidy_data.txt")
 }
 
